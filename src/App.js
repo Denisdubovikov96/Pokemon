e5918226e4acaa2eec79c50f7335648cbf4c597a
Denis Dubovikov <denisdubovikov96@gmail.com>
2020-11-23 19:30:25 +0200

    срфтпу

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Pokemon from './Pokemon/Pokemon'
-
+import React, { Component } from "react";
+import "./App.css";
+import Pokemon from "./Pokemon/Pokemon";
+import imgs from "./img";
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-
-				<h1>Pokemon</h1>
-				<Pokemon />
-			</div>
-		);
-	}
-
+  render() {
+    console.log(imgs);
+    return (
+      <div className="App">
+        <h1>Pokemon</h1>
+        {imgs.map((img) => {
+          // console.log(img.default);
+          return <Pokemon img={img.default} />;
+        })}
+        <Pokemon />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Pokemon/Pokemon.js
+++ src/Pokemon/Pokemon.js
@@ -9,53 +9,55 @@
 // 	</div>
 // )
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+// import img1 from "../img/001.png";
+// import imgs from "../img";
 
 const useStyles = makeStyles({
-	root: {
-		maxWidth: 345,
-	},
+  root: {
+    maxWidth: 345,
+  },
 });
 
-export default function ImgMediaCard() {
-	const classes = useStyles();
-
-	return (
-		<Card className={classes.root}>
-			<CardActionArea>
-				<CardMedia
-					component="img"
-					alt="Contemplative Reptile"
-					height="140"
-					image="./001.png"
-					title="Contemplative Reptile"
-				/>
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="h2">
-						Lizard
+export default function ImgMediaCard({ img }) {
+  const classes = useStyles();
+  console.log(img);
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={img}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
           </Typography>
-					<Typography variant="body2" color="textSecondary" component="p">
-						Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-						across all continents except Antarctica
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
           </Typography>
-				</CardContent>
-			</CardActionArea>
-			<CardActions>
-				<Button size="small" color="primary">
-					Share
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
         </Button>
-				<Button size="small" color="primary">
-					Learn More
+        <Button size="small" color="primary">
+          Learn More
         </Button>
-			</CardActions>
-		</Card>
-	);
+      </CardActions>
+    </Card>
+  );
 }
